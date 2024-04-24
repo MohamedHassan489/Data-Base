4e13805c2e58f9abdbe43fd543362056e396e9b7
youssefalbertt <youssefalbert2003@gmail.com>
2024-04-24 20:04:16 +0200

    add raw materials file

---
+++ raw_materials.py
@@ -0,0 +1,43 @@
+import pyodbc
+import customtkinter as ctk
+
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+app.geometry("300x500")
+app.title('Raw Materials')
+
+entry_materialid = ctk.CTkEntry(app, placeholder_text = "Material id")
+entry_materialid.place(relx=0.2 , rely=0.2)
+
+entry_materialname = ctk.CTkEntry(app, placeholder_text = "Name")
+entry_materialname.place(relx=0.2 , rely=0.3)
+
+
+def insert():
+    try:
+        connection =pyodbc.connect('DRIVER={SQL Server};'+
+                                   'SERVER=YOUSSEFALBERT;'+
+                                   'DATABASE=MedicineFactoryDB;'+
+                                   'Trusted_connection= True;'
+        )
+        connection.autocommit= True
+        connection.execute(f"insert into Raw_Materials values"+
+                           f"( {entry_materialid.get()} , '{entry_materialname.get()}')")
+        info_label.configure(text="INSERT COMPLETED!")
+
+    except pyodbc.Error as ex:
+        print('Connection failed', ex)
+        info_label.configure(text = "INSERT FAILED!")
+
+insert_button = ctk.CTkButton(app, border_spacing=2,text="INSERT", command =insert, fg_color="green")
+insert_button.place(relx=0.2 , rely=0.8)
+
+table_label = ctk.CTkLabel(app , text ="Add a raw material")
+table_label.place(relx=0.2 , rely=0.1)
+
+info_label = ctk.CTkLabel(app , text ="")
+info_label.place(relx=0.2 , rely=0.9)
+
+app.mainloop()
